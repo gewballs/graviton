7b06e68859373c1c790f6612f67f7bb4f74c1c10
gewballs <gewballs@hotmail.com>
2015-06-27 14:02:00 -0400

    Added gravity gauge, animated grass, put temporary gravity control in place, tied gradient to gauge, decided against animating fish but may reconsider.

---
+++ gravity.asm
@@ -0,0 +1,38 @@
+            ;#Code w {Gravity}
+			;PHP
+			;REP #$30
+			;
+            ;# Gauge ===============
+            ;LDA joy              // !!! DEBUG !!!
+			;BIT #$0400           // Down
+			;BEQ {+Up}
+			;LDA gravity0
+			;DEC A
+			;BRA {+Clamp}
+;{+Up}      ;BIT #$0800
+            ;BEQ {+Neutral}
+			;LDA gravity0
+            ;INC A
+			;BRA {+Clamp}
+;{+Neutral} ;LDA gravity0
+			;
+;{+Clamp}   ;BMI {+Minus}
+			;CMP #$0020
+			;BCC {+Write}
+			;LDA #$001F
+			;BRA {+Write}
+;{+Minus}   ;CMP #$FFE1
+			;BCS {+Write}
+			;LDA #$FFE1
+;{+Write}   ;STA gravity1
+			;
+			;LDA gravity1
+			;STA gravity0
+			;
+			;PLP
+			;RTS
+
+            ;# ===============================================================//
+            ;#                                                                //
+            ;# ===============================================================//
+
